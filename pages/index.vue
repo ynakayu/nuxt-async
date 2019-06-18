e76007d7ac5e5bfd050d229b9ac9d3f821605518
ynakayu <9111754+ynakayu@users.noreply.github.com>
2019-06-18 17:36:39 +0900

    get-data

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,22 @@
 <template>
   <section class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        nuxt-async
-      </h1>
-      <h2 class="subtitle">
-        My super Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
+      {{ users[0].id }}, {{ users[0].name }}
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+const axios = require('axios')
+let url = 'https://jsonplaceholder.typicode.com/users'
 
 export default {
-  components: {
-    Logo
+  asyncData({ params }) {
+    return axios.get(url) 
+      .then((res) => {
+        return { users: res.data }
+      })
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
